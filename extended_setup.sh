265da882feb331d26772fb3ce8a93c0849e36503
kdkocev <kamen@hacksoft.io>
2024-11-03 14:36:06 +0200

    Add docker setup

--- extended_setup.sh
+++ extended_setup.sh
@@ -5,3 +5,7 @@ git config --global user.name $gitusername
 
 read -p "Enter your git email: " gitemail
 git config --global user.email $gitemail
+
+echo "Generate ssh key"
+ssh-keygen -t ed25519 -C "$gitemail"
+
--- setup.sh
+++ setup.sh
@@ -37,3 +37,45 @@ echo 'deb [signed-by=/usr/share/keyrings/sublimehq.gpg] https://download.sublime
 # Install sublime text
 sudo apt update
 sudo apt install sublime-text
+# TODO: setup the color scheme and Typescript and Python packages using cli
+
+# Install google chrome
+echo "Install Google Chrome"
+curl -o ~/Downloads/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb
+
+echo "Install pv"
+sudo apt install pv
+
+echo "Install nvm"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+echo "Install docker"
+# Remove any previously installed docker packages
+# for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+# Install the latest
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Setup the current user in the docker group so that scripts can be executed without sudo
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+# "Relog" the user to use the new group permission
+su - $USER
+sudo service docker restart
+
+echo "Add alias dock"
+echo 'alias dock="docker compose run django"' >> ~/.zshrc
+echo "Add alias sail"
+echo 'alias sail="./vendor/bin/sail"' >> ~/.zshrc
